edabe4a82ec73abf0d32c57938d1fb54b6ac9ef3
Artem Sydorenko <artem.sydorenko@yalantis.com>
2016-06-24 14:16:03 +0300

    Fix crash: init(coder:) not implemented

--- Classes/ScrollableGraphView.swift
+++ Classes/ScrollableGraphView.swift
@@ -241,7 +241,7 @@ import UIKit
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     deinit {
