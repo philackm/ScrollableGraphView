d0a3218940d4a062f4d28ddad141042799d90db1
philackm <philackm@icloud.com>
2017-07-07 15:47:39 +1000

    Add files to interface builder example and upgrade settings

--- graphview_example_ib/GraphView/GraphViewController.swift
+++ graphview_example_ib/GraphView/GraphViewController.swift
@@ -16,7 +16,8 @@ class GraphViewController: UIViewController {
         guard let data = data, let labels = labels else {
             return
         }
-        graphView?.set(data: data, withLabels: labels)
+        
+        //graphView?.set(data: data, withLabels: labels)
     }
 
 }
