32c1015c21a05a56c43a7bdec3a55bf7b48e0ee6
philackm <philackm@icloud.com>
2017-07-09 19:36:31 +1000

    Add carthage support

---
+++ carthage/ScrollableGraphView/ScrollableGraphView/ScrollableGraphView.h
@@ -0,0 +1,16 @@
+//
+//  ScrollableGraphView.h
+//  ScrollableGraphView
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollableGraphView.
+FOUNDATION_EXPORT double ScrollableGraphViewVersionNumber;
+
+//! Project version string for ScrollableGraphView.
+FOUNDATION_EXPORT const unsigned char ScrollableGraphViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableGraphView/PublicHeader.h>
+
+
